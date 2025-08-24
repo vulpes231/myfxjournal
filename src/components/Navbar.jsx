0b10b948325ed2e9c7f7d0fb292e0a6d771dbc8e
Vulpes231 <whykayshot@gmail.com>
2025-08-24 18:25:01 +0100

    re init

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,63 +2,63 @@ import React, { useState } from "react";
 import { links } from "../constants";
 import { styles } from "../styles";
 import {
-  MdAddChart,
-  MdClose,
-  MdDarkMode,
-  MdLightMode,
-  MdMenu,
+	MdAddChart,
+	MdClose,
+	MdDarkMode,
+	MdLightMode,
+	MdMenu,
 } from "react-icons/md";
 
 const Navbar = ({ mode, toggleMode }) => {
-  const [toggle, setToggle] = useState(false);
+	const [toggle, setToggle] = useState(false);
 
-  const navLinks = links.map((link) => {
-    return (
-      <li key={link.id}>
-        <a href="">{link.name}</a>
-      </li>
-    );
-  });
+	const navLinks = links.map((link) => {
+		return (
+			<li key={link.id}>
+				<a href="">{link.name}</a>
+			</li>
+		);
+	});
 
-  const handleToggle = () => {
-    setToggle((prev) => !prev);
-  };
+	const handleToggle = () => {
+		setToggle((prev) => !prev);
+	};
 
-  return (
-    <header
-      className={
-        mode
-          ? `${styles.primary.bgColor} ${styles.primary.textColor} ${styles.primary.padding} w-full`
-          : `${styles.secondary.bgColor} ${styles.secondary.textColor} ${styles.primary.padding} w-full`
-      }
-    >
-      <nav className={`flex justify-between items-center`}>
-        <span
-          className={
-            mode
-              ? `flex text-xl items-center gap-2 ${styles.primary.textColor}`
-              : `flex text-xl items-center gap-2 ${styles.secondary.textColor}`
-          }
-        >
-          <MdAddChart />
-          <h1 className="font-bold uppercase">vfx</h1>
-        </span>
-        <ul
-          className={
-            toggle
-              ? `flex flex-col gap-4 absolute top-[80px] left-0 w-full capitalize `
-              : `hidden md:flex gap-4 capitalize`
-          }
-        >
-          {navLinks}
-        </ul>
-        <span
-          className={`hidden md:flex items-center cursor-pointer ${styles.secondary.gap}`}
-        >
-          <span onClick={toggleMode}>
-            {mode ? <MdDarkMode /> : <MdLightMode />}
-          </span>
-          {/* <button
+	return (
+		<header
+			className={
+				mode
+					? `${styles.primary.bgColor} ${styles.primary.textColor} ${styles.primary.padding} w-full`
+					: `${styles.secondary.bgColor} ${styles.secondary.textColor} ${styles.primary.padding} w-full`
+			}
+		>
+			<nav className={`flex justify-between items-center`}>
+				<span
+					className={
+						mode
+							? `flex text-xl items-center gap-2 ${styles.primary.textColor}`
+							: `flex text-xl items-center gap-2 ${styles.secondary.textColor}`
+					}
+				>
+					<MdAddChart />
+					<h1 className="font-bold uppercase">vfx</h1>
+				</span>
+				<ul
+					className={
+						toggle
+							? `flex flex-col gap-4 absolute top-[80px] left-0 w-full capitalize `
+							: `hidden md:flex gap-4 capitalize`
+					}
+				>
+					{navLinks}
+				</ul>
+				<span
+					className={`hidden md:flex items-center cursor-pointer ${styles.secondary.gap}`}
+				>
+					<span onClick={toggleMode}>
+						{mode ? <MdDarkMode /> : <MdLightMode />}
+					</span>
+					{/* <button
             className={
               mode
                 ? `${styles.primary.border} text-white p-2 cursor-pointer `
@@ -67,14 +67,14 @@ const Navbar = ({ mode, toggleMode }) => {
           >
             Create Strategy
           </button> */}
-        </span>
-        {/* hamburger */}
-        <span onClick={handleToggle} className="sm:hidden">
-          {!toggle ? <MdMenu /> : <MdClose />}{" "}
-        </span>
-      </nav>
-    </header>
-  );
+				</span>
+				{/* hamburger */}
+				<span onClick={handleToggle} className="sm:hidden">
+					{!toggle ? <MdMenu /> : <MdClose />}{" "}
+				</span>
+			</nav>
+		</header>
+	);
 };
 
 export default Navbar;
